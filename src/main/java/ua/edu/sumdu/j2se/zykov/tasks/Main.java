424e607721ff354ae05ab7fb7e11c7b2f8693e28
Maxuha <zykovmaksym@gmail.com>
2020-01-28 16:49:22 +0200

    Fix format code.

--- src/main/java/ua/edu/sumdu/j2se/zykov/tasks/Main.java
+++ src/main/java/ua/edu/sumdu/j2se/zykov/tasks/Main.java
@@ -18,21 +18,21 @@ import java.io.IOException;
  * class main - class start point program.
  */
 public class Main {
-/**
-* start point is program.
-* @param args as array arguments run program.
-*/
-public static void main(String[] args) {
-    Logger logger = LoggerFactory.getLogger(Main.class);
-    AbstractTaskList taskList = new ArrayTaskList();
-    try {
-        TaskIO.readText(taskList, new File("tasks.json"));
-        logger.info("Loaded tasks.json file");
-    } catch (IOException e) {
-        logger.error("Failed to load tasks.json file");
+    /**
+    * start point is program.
+    * @param args as array arguments run program.
+    */
+    public static void main(String[] args) {
+        Logger logger = LoggerFactory.getLogger(Main.class);
+        AbstractTaskList taskList = new ArrayTaskList();
+        try {
+            TaskIO.readText(taskList, new File("tasks.json"));
+            logger.info("Loaded tasks.json file");
+        } catch (IOException e) {
+            logger.error("Failed to load tasks.json file");
+        }
+        View view = new ConsoleView();
+        Controller controller = new ConsoleController(view, taskList);
+        controller.process(Actions.MAIN_MENU);
     }
-    View view = new ConsoleView();
-    Controller controller = new ConsoleController(view, taskList);
-    controller.process(Actions.MAIN_MENU);
-}
 }
